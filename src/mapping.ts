978b9e957787eb03fe2d54d5e8ea4869a41de466
anoushk1234 <anoushka1828@gmail.com>
2022-01-27 11:46:45 +0530

    no

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,29 +3,29 @@ import { DaoFactory, tokencreated } from "../generated/DaoFactory/DaoFactory";
 import { Transfer, MintCall } from "../generated/templates/InstaDao/InstaDao";
 import { TokenEntity, TokenTransferEntity } from "../generated/schema";
 
-export function handletokencreated(event: tokencreated): void {
+export function handletokencreated(eventOne: tokencreated): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = TokenEntity.load(event.transaction.hash.toHexString());
+  let entityOne = TokenEntity.load(eventOne.transaction.hash.toHexString());
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new TokenEntity(event.transaction.hash.toHexString());
+  if (!entityOne) {
+    entityOne = new TokenEntity(eventOne.transaction.hash.toHexString());
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0);
+    // entityOne fields can be set using simple assignments
+    entityOne.count = BigInt.fromI32(0);
   }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1);
+  entityOne.count = entityOne.count + BigInt.fromI32(1);
 
-  // Entity fields can be set based on event parameters
-  entity.tokenaddress = event.params.tokenaddress;
-  entity.creator = event.params.creator;
+  // entityOne fields can be set based on eventOne parameters
+  entityOne.tokenaddress = eventOne.params.tokenaddress;
+  entityOne.creator = eventOne.params.creator;
 
   // Entities can be written to the store with `.save()`
-  entity.save();
+  entityOne.save();
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -34,7 +34,7 @@ export function handletokencreated(event: tokencreated): void {
   // unchanged, allowing for partial updates to be applied.
 
   // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
+  // example, the contract that has emitted the eventOne can be connected to
   // with:
   //
   // let contract = Contract.bind(event.address)
@@ -46,20 +46,20 @@ export function handletokencreated(event: tokencreated): void {
   // - contract.getContract(...)
 }
 
-export function handleTransfer(event: Transfer): void {
-  let entity = TokenTransferEntity.load(event.transaction.hash.toHexString());
+export function handleTransfer(eventTwo: Transfer): void {
+  let entityTwo = new TokenTransferEntity(eventTwo.transaction.hash.toHex());
 
-  if (!entity) {
-    entity = new TokenTransferEntity(event.transaction.hash.toHexString());
-    entity.count = BigInt.fromI32(0);
+  if (!entityTwo) {
+    entityTwo = new TokenTransferEntity(eventTwo.transaction.hash.toHex());
+    entityTwo.count = BigInt.fromI32(0);
   }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1);
-  // entity.from = event.params.from;
-  entity.to = event.params.to.toHexString();
-  entity.amt = event.params.value;
-  entity.save();
+  entityTwo.count = entityTwo.count + BigInt.fromI32(1);
+  // entityTwo.from = eventTwo.params.from;
+  entityTwo.to = eventTwo.params.to.toHexString();
+  entityTwo.amt = eventTwo.params.value;
+  entityTwo.save();
 }
 
 // export function handleMintCall(call: MintCall): void {
